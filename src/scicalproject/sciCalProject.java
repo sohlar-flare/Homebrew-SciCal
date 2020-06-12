6514b9ab214fe7162a3d083f1e6fd348b8b6e025
sohlar-flare <66833867+sohlar-flare@users.noreply.github.com>
2020-06-12 23:53:53 +0800

    First initialization

---
+++ src/scicalproject/sciCalProject.java
@@ -0,0 +1,36 @@
+
+package scicalproject;
+
+import java.util.Scanner;
+
+
+public class sciCalProject {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a username: ");
+        
+        String name = userInput.nextLine();
+        System.out.println("Hello, " + name + ", have a nice day!");
+        
+        System.out.println("Please select a function: ");
+        System.out.println("1 = graphing, 2 = system of linear equations, 3 = matrices");
+        
+        int functionResponse = userInput.nextInt();
+        
+        switch(functionResponse)
+        {
+            case (1):
+               System.out.println("Still under maintenance");
+               break;
+            case 2:
+               System.out.println("Still under maintenance");  
+               break;
+            case 3:
+               System.out.println("Still under maintenance");
+               break;
+            
+        }   
+    }
+    
+}
